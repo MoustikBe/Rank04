4f00d74ede4dc7854307c3a1807c2a934d5ec892
Maxence Isaac Castiau <misaac-c@c1r14s6.42madrid.com>
2025-03-04 12:51:00 +0100

    good version of the microshell home made

--- micro.c
+++ micro.c
@@ -1,286 +1,88 @@
-
-// Declaration de la strucute utiliser, array de struct contenant char **cmd
-// et un id -> 1 pour | -> 2 pour ; -> 0 pour le reste, ? Comment connaitre l'id. Regarder le | ou le ; d'apres et si pas alors mettre 0. 
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-
-typedef struct t_shell
+#include <stdlib.h>
+void	err(char *str)
 {
-	char **cmd;
-	int id;
-	int pipe_needed;
-}	t_shell;
-
-typedef struct t_utils
-{
-	int fd[2];
-}	t_utils;
-
-int ft_strlen(char *str)
-{
-	int i = 0;
-
-	while(str[i])
-		i++;
-	return(i);
+	while (*str)
+		write(2, str++, 1);
 }
-
-int mem_struct(int argc, char **argv)
+int	cd(char **argv, int i)
 {
-	int i = 1;
-	int len_mem = 0;
-
-	if(argc != 1)
-		len_mem++;
-	while(i < argc)
+	if (i != 2)
 	{
-		if(strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0)
-		{
-			if(argv[i + 1])
-				len_mem++;
-		}
-		i++;
+		err("error: cd: bad arguments\n");
+		return (1);
 	}
-	len_mem++;
-	return(len_mem);
+	if (chdir(argv[1]) == -1)
+	{
+		err("error: cd: cannot change directory to ");
+		err(argv[1]);
+		err("\n");
+		return (1);
+	}
+	return (0);
 }
-
-void mem_for_cmd(t_shell *shell, char **argv, int len, int i, int index)
+void	set_pipe(int has_pipe, int *fd, int end)
 {
-	int i_cmd = 0;
-
-	if(!argv[i + 1] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
-			i++;
-	shell[index].cmd[len] = NULL;
-	while(len)
+	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1
+			|| close(fd[1]) == -1))
 	{
-		//printf("len  -> %d | argv[i - 1] -> %s | len_cmd -> %d\n", len -1, argv[i - 1], ft_strlen(argv[i -1]) + 1);
-		//shell[index].cmd[len - 1] = malloc(sizeof(char) * (ft_strlen(argv[i - 1]) + 1));
-		shell[index].cmd[len - 1] = strdup(argv[i - 1]);
-		//printf("Valeur copier -> %s\n", shell[index].cmd[len - 1]);
-		i_cmd++;
-		len--;
-		i--;
-	}
-
-}
-
-
-
-void mem_for_shell(t_shell *shell, char **argv, int argc)
-{
-	int i = 1;
-	int len = 0;
-	int index = 0;
-
-	if(argc == 1)
-		return ;
-	while(i < argc)
-	{
-		if(strcmp(argv[i], "|") == 0 || strcmp(argv[i], ";") == 0 || !argv[i + 1])
-		{
-			if(!argv[i + 1] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
-			{
-				shell[index].id = 0;
-				len++;
-			}
-			else if(strcmp(argv[i], "|") == 0)
-				shell[index].id = 1;
-			else if(strcmp(argv[i], ";") == 0)
-				shell[index].id = 2;
-			if(index > 0)
-			{
-				if(shell[index - 1].id == 1 && strcmp(argv[i], "|") != 0)
-				{
-					shell[index].id = 1;
-					shell[index].pipe_needed = 1;
-				}
-				else 
-					shell[index].pipe_needed = 0;
-			}
-			//printf("len + 1 -> %d\n", len + 1);
-			shell[index].cmd = malloc(sizeof(char *) * (len + 1));
-			mem_for_cmd(shell, argv, len, i, index);
-			index++;
-			len = 0;
-		}
-		else 
-			len++;
-		i++;
-	}
-	return ;
-}
-
-
-void print_struct(t_shell *shell, int mem)
-{
-	int i = 0;
-	int j = 0;
-
-	if (!shell)
-	{
-		printf("Erreur: shell est NULL\n");
-		return;
-	}
-
-	while (i < mem)
-	{
-		if (!shell[i].cmd)
-		{
-			printf("shell[%d].cmd est NULL\n", i);
-			i++;
-			continue;
-		}
-
-		j = 0;
-		while (shell[i].cmd[j])
-		{
-			printf("shell[%d].cmd[%d]: %s\n", i, j, shell[i].cmd[j]);
-			j++;
-		}
-		printf("shell[%d].id = %d \n", i, shell[i].id);
-		printf("shell[%d].pipe_needed = %d \n", i, shell[i].pipe_needed);
-		i++;
+		err("error: fatal\n");
+		exit(1);
 	}
 }
-
-void exec_nothing(t_shell *shell, char **envp, int i)
+int	exec(char **argv, int i, char **envp)
 {
-	execve(shell[i].cmd[0], shell[i].cmd, envp);
-}
-
-
-void exec_more(t_shell *shell, char **envp, int i)
-{
-	pid_t pid;
-	int status;
-
+	int	has_pipe;
+	int	fd[2];
+	int	pid;
+	int	status;
+	has_pipe = argv[i] && !strcmp(argv[i], "|");
+	if (!has_pipe && !strcmp(*argv, "cd"))
+		return (cd(argv, i));
+	if (has_pipe && pipe(fd) == -1)
+	{
+		err("error: fatal\n");
+		exit(1);
+	}
 	pid = fork();
-	if(pid == 0)
-		execve(shell[i].cmd[0], shell[i].cmd, envp);
-	else
-		waitpid(-1, &status, 0);
-	return ;
-
-}
-
-void exec_pipe(t_utils *utils, t_shell *shell, char **envp, int i, int pipe_needed)
-{
-    int new_pipe[2];
-
-    // Créez un nouveau pipe si la commande courante doit écrire vers un pipe
-    if (pipe_needed)
-    {
-        if (pipe(new_pipe) < 0)
-        {
-            perror("pipe");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        // Dans le processus enfant
-
-        // Si la commande précédente a écrit dans un pipe, redirigez l'entrée standard
-        if (utils->fd[0] != -1)
-        {
-            dup2(utils->fd[0], STDIN_FILENO);
-            close(utils->fd[0]);
-        }
-
-        // Si la commande courante est suivie d'un pipe, redirigez la sortie standard
-        if (pipe_needed)
-        {
-            dup2(new_pipe[1], STDOUT_FILENO);
-            close(new_pipe[0]);
-            close(new_pipe[1]);
-        }
-
-        // Exécutez la commande
-        execve(shell[i].cmd[0], shell[i].cmd, envp);
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        // Dans le processus parent
-
-        // Si un pipe a été créé, fermez l'extrémité d'écriture dans le parent
-        if (pipe_needed)
-            close(new_pipe[1]);
-
-        // Si un pipe existait déjà pour la lecture (issu de la commande précédente), fermez-le
-        if (utils->fd[0] != -1)
-            close(utils->fd[0]);
-
-        // Si la commande courante est suivie d'un pipe, sauvegardez le nouveau descripteur de lecture
-        if (pipe_needed)
-            utils->fd[0] = new_pipe[0];
-        else
-            utils->fd[0] = -1; // Réinitialisez si aucun pipe n'est utilisé
-
-        waitpid(pid, NULL, 0);
-    }
-}
-
-
-
-void execution(t_utils *utils, t_shell *shell, char **envp, int mem)
-{
-	int	i = 0;
-	
-	if(!shell)
-		return ;
-	while (i < mem)
+	if (pid == -1)
 	{
-		if(!shell[i].cmd)
-			return ;
-		// \0
-		else if(shell[i].id == 0)
-			exec_nothing(shell, envp, i);
-		// |
-		else if(shell[i].id == 1)
-			exec_pipe(utils, shell, envp, i, shell[i].pipe_needed);
-		// ;
-		else if(shell[i].id == 2)
-			exec_more(shell, envp, i);
-		i++;
+		err("error: fatal\n");
+		exit(1);
 	}
-	
-
+	if (!pid)
+	{
+		argv[i] = 0;
+		set_pipe(has_pipe, fd, 1);
+		if (!strcmp(*argv, "cd"))
+			exit(cd(argv, i));
+		execve(*argv, argv, envp);
+		err("error: cannot execute ");
+		err(*argv);
+		err("\n");
+		exit(1);
+	}
+	waitpid(pid, &status, 0);
+	set_pipe(has_pipe, fd, 0);
+	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
-
-
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	// Declarer et initier la struct
-	t_shell *shell;
-	t_utils *utils;
-	int mem;
-	// Faire une fonction qui calcule la memoire necessaire
-	mem = mem_struct(argc, argv);
-	utils = malloc(sizeof(t_utils));
-	//printf("L'alloc de mem est egal a -> %d\n", mem);
-	shell = malloc(sizeof(t_shell) * mem);
-	shell[mem - 1].cmd = NULL;
-	shell[mem - 1].id  = 0;
-	mem_for_shell(shell, argv, argc);
-
-	//Print la struct final.
-	//print_struct(shell, mem);
-	// jusque la parfait 
-	// Executer
-	execution(utils, shell, envp, mem);
-	free(utils);
+	int	status;
+	int	i;
+	(void)argc;
+	i = 0;
+	status = 0;
+	while (argv[i])
+	{
+		argv += i + 1;
+		i = 0;
+		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			i++;
+		if (i)
+			status = exec(argv, i, envp);
+	}
+	return (status);
 }
--- microshell.c
+++ microshell.c
@@ -2,16 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 typedef struct t_shell
 {
 	char 	**cmd;
 	int		id;
 	int		pipe_before;
+	int		pipe_after;
+	int 	prev_fd;
+	int		pipe_fd[2];
 }	t_shell;
 
-int nb_cmd(int argc, char **argv)
+int ft_strlen(char *cmd)
+{
+	int i = 0;
+	while (cmd[i])
+		i++;
+	return(i);
+}
+
+int nb_cmd(char **argv)
 {
 	int i = 0;
 	int len = 1;
@@ -22,12 +34,28 @@ int nb_cmd(int argc, char **argv)
 			len++;
 		i++;
 	}
-	printf("len -> %d\n", len);
+	//printf("len -> %d\n", len);
 	return(len);
 }
 
+int	cd(char **argv, int i)
+{
+	if (i != 2)
+	{
+		write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
+		return (1);
+	}
+	if (chdir(argv[1]) == -1)
+	{
+		write(2, "error: cd: cannot change directory to", ft_strlen("error: cd: cannot change directory to"));
+		write(2, argv[1], ft_strlen(argv[1]));
+		write(2, "\n", 1);
+		return (1);
+	}
+	return (0);
+}
 
-int	alloc_cmd(t_shell *shell, char **argv, int index, int i)
+int	alloc_cmd(char **argv, int i)
 {
 	int len = 0;
 	while (strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0 && argv[i + 1])
@@ -37,7 +65,7 @@ int	alloc_cmd(t_shell *shell, char **argv, int index, int i)
 	}
 	if(!argv[i + 1])
 		len++;
-	printf("len_char -> %d\n", len);
+	//printf("len_char -> %d\n", len);
 	return(len);
 }
 
@@ -53,7 +81,7 @@ void store_in_struct(t_shell *shell, char **argv)
 			i++;
 		else
 		{
-			shell[index].cmd = malloc(sizeof(char *) * alloc_cmd(shell, argv, index, i) + 1);
+			shell[index].cmd = malloc(sizeof(char *) * alloc_cmd(argv, i) + 1);
 			if(strcmp(argv[i - 1], "|") == 0)
 				shell[index].pipe_before = 1;
 			else
@@ -99,37 +127,110 @@ void print_all(t_shell *shell)
 		i++;
 	}
 }
-/*
-void exec_dot_come(char **cmd, )
+
+void exec_dot_come(char **cmd, char **envp)
 {
+	pid_t	pid;
+	int		status;
 
+	pid = fork();
+	if(pid == 0)
+		execve(cmd[0], cmd, envp);
+	else 
+		waitpid(-1, &status, 0);
+	// FAIL CASE // 
+	return ;
+}
 
+int len_array(char **cmd)
+{
+	int i = 0;
+
+	while (cmd[i])
+		i++;
+	return(i);
 
 }
-*/
 
 void exec(t_shell *shell, char **envp)
 {
-	int i = 0;
-	int i_ = 0;
+    int i = 0;
+    int fd[2];
+    int status;
+    pid_t pid;
 
+	if (shell[0].cmd[0] == NULL)
+		return ;
 	while (shell[i].cmd != NULL)
-	{
-		i_ = 0;
-		if(shell[i].id == 0)
-			execve(shell[i].cmd[0], shell[i].cmd, envp);
-		else if(shell[i].id == 2);
-			exec_dot_coma(shell.cmd);
-		// exec basique.
+    {
+        // Si la commande doit envoyer sa sortie dans un pipe,
+        // créez le pipe avant le fork.
+        if(shell[i].id == 1) {
+            if (pipe(fd) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	}
+        pid = fork();
+        if(pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if(pid == 0)  // Processus enfant
+        {
+			if (shell[i].cmd[0] == NULL)
+				return ;
+            if(shell[i].pipe_before) {
+                // Rediriger l'entrée standard vers le pipe précédent
+                dup2(shell[i].prev_fd, STDIN_FILENO);
+                close(shell[i].prev_fd);
+            }
+            if(shell[i].id == 1) {
+                // Rediriger la sortie standard vers le pipe nouvellement créé
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[0]);
+                close(fd[1]);
+            }
+			if(strcmp(shell[i].cmd[0], "cd") == 0)
+				exit(cd(shell[i].cmd, len_array(shell[i].cmd)));
+            execve(shell[i].cmd[0], shell[i].cmd, envp);
+            write(2, "error: cannot execute ", ft_strlen("error: cannot execute "));
+			write(2, shell[i].cmd[0], ft_strlen(shell[i].cmd[0]));
+			write(2, "\n", 1);
+            exit(EXIT_FAILURE);
+        }
+        else  // Processus parent
+        {
+            // Si un pipe précède, fermer le descripteur de lecture dans le parent
+            if(shell[i].pipe_before)
+                close(shell[i].prev_fd);
+            // Si la commande a créé un pipe pour la sortie,
+            // fermer le côté écriture et conserver le côté lecture pour la prochaine commande.
+            if(shell[i].id == 1)
+            {
+                close(fd[1]);
+                // On stocke fd[0] dans la structure de la commande suivante pour pipe_before
+                shell[i+1].prev_fd = fd[0];
+                shell[i+1].pipe_before = 1;
+            }
+            waitpid(pid, &status, 0);
+        }
+        i++;
+    }
 }
+
+
 
 int main(int argc, char **argv, char **envp)
 {
 	t_shell *shell;
 
-	shell = malloc(sizeof(t_shell) * nb_cmd(argc, argv) + 1); 
+	if(argc < 2)
+		return(printf("ERROR \n"));
+	//printf("aloc -> %d\n", aloc_shell);
+	shell = malloc(sizeof(t_shell) * nb_cmd(argv) + 1); 
 	// Etape 1. Stockage && identification.
 	store_in_struct(shell, argv);
 	//print_all(shell);
